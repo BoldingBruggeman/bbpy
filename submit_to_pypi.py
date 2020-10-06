43312f5ded680281366d2a2c8dc461b49c45bb00
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-10-06 14:00:25 +0100

    updated gotmgui; allow submit of whl directly to pypi

--- submit_to_pypi.py
+++ submit_to_pypi.py
@@ -7,10 +7,10 @@ import subprocess
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('dir')
+parser.add_argument('target')
 args = parser.parse_args()
 
-for path in glob.glob(args.dir):
+for path in glob.glob(args.target):
     if os.path.isdir(path):
         for tmpname in ('build', 'dist'):
             tmpdir = os.path.join(path, tmpname)
@@ -18,4 +18,7 @@ for path in glob.glob(args.dir):
                 shutil.rmtree(tmpdir)
         subprocess.check_call(['python', 'setup.py', 'bdist_wheel'], cwd=path)
         subprocess.check_call(['twine', 'upload', 'dist/*'], cwd=path)
+    elif os.path.isfile(path):
+        assert path.lower().endswith('.whl')
+        subprocess.check_call(['twine', 'upload', path])
 
